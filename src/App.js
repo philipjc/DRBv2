a4c98016a6b2d205f1555f0fad904df8ef58e1bb
philipjc <philipcox83@gmail.com>
2017-07-28 15:14:53 +0100

    router updates.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {
+  HashRouter,
+  Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Home from './Routes/Home/Home';
-import Main from './Routes/Main/Main';
+import Services from './Routes/Services/Services';
+import Management from './Routes/Management/Management';
 
 import './App.css';
 
@@ -8,17 +17,20 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-
   }
 
   render() {
     return (
       <div className='App'>
-
-        <Main />
-
+        <main>
+          <Switch>
+            <Route exact path='/home' component={Home} />
+            <Route path='/services' component={Services} />
+          </Switch>
+        </main>
       </div>
     );
+
   }
 }
 
